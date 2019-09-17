3d01fd4269ce32dd3b330bbbf4a4f184408be26b
Aki Väliaho <akivvaliaho@gmail.com>
2019-09-17 14:28:16 +0300

    Base architecture for swappable i18n with resources files
    ?lang parameter in api requests

--- src/main/java/com/valiaho/rockpaperscissors/playaround/GameController.java
+++ src/main/java/com/valiaho/rockpaperscissors/playaround/GameController.java
@@ -1,6 +1,9 @@
 package com.valiaho.rockpaperscissors.playaround;
 
 import com.valiaho.rockpaperscissors.domain.GameEntity;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.Info;
+import io.swagger.annotations.SwaggerDefinition;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
  * Created by akivv on 16.9.2019.
  */
 @RestController("/api/v1")
+@SwaggerDefinition(info = @Info(
+        title = "game api", version = "1", description = "Allows for a round of rock paper scissors, i18n enabled with the lang property (eg. ?lang=en"))
 class GameController {
     @Autowired
     private GameService gameService;
