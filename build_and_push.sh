82036f83a5c82628e673365462008aef8c57f6a9
Aki Väliaho <akivvaliaho@gmail.com>
2019-09-19 16:22:19 +0300

    Tuning docker container build in script

--- build_and_push.sh
+++ build_and_push.sh
@@ -3,7 +3,8 @@ mvn clean install
 if [[ "$?" -ne 1 ]] ; then
   echo 'Build is okay, pushing';
   heroku container:login
-  heroku container:push web --app rockpaperscissorsaki
+  docker build -t registry.heroku.com/rockpaperscissorsaki/web .
+  docker push registry.heroku.com/rockpaperscissorsaki/web
   heroku container:release web --app rockpaperscissorsaki
   else echo "Build fails, don't push"; exit 1
 fi
