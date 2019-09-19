aae139cce8fe23220bb912e80b9d5ac598a5f972
Aki Väliaho <akivvaliaho@gmail.com>
2019-09-19 14:09:25 +0300

    Add a vuejs frontend

--- src/main/java/com/valiaho/rockpaperscissors/playaround/GameController.java
+++ src/main/java/com/valiaho/rockpaperscissors/playaround/GameController.java
@@ -1,7 +1,6 @@
 package com.valiaho.rockpaperscissors.playaround;
 
 import com.valiaho.rockpaperscissors.domain.GameEntity;
-import io.swagger.annotations.Api;
 import io.swagger.annotations.Info;
 import io.swagger.annotations.SwaggerDefinition;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,8 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
  * Created by akivv on 16.9.2019.
  */
 @RestController("/api/v1")
-@SwaggerDefinition(info = @Info(
-        title = "game api", version = "1", description = "Allows for a round of rock paper scissors, i18n enabled with the lang property (eg. ?lang=en"))
+@SwaggerDefinition(info = @Info(title = "Game controller implementation", version = "1.0"))
 class GameController {
     @Autowired
     private GameService gameService;
